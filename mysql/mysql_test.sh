b19b4621767ad9d5703ec4aa4b956b810f29afe9
root <root@vh107.hoster.by>
2022-04-04 13:46:16 +0300

    add files dir

---
+++ mysql/mysql_test.sh
@@ -0,0 +1,14 @@
+FILE=/var/run/mysqld/mysqld.pid
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "$FILE not found"
+fi
+sleep 5
+FILE_DB=/var/lib/mysql/db/mwg8_actor.ibd
+if [ -f "$FILE_DB" ]; then
+    echo "$FILE_DB exists."
+else
+    echo "$FILE_DB not found"
+fi
+
