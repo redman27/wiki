ed2006d67f67902197ffa3e73611183b8ae4cb3d
root <root@vh107.hoster.by>
2022-04-05 10:35:07 +0300

    add manual jenkins

---
+++ post.sh
@@ -0,0 +1,2 @@
+mv LocalSettings.php LocalSettings.php_bak && mv LocalSettings.php_ LocalSettings.php
+cd /home/otpotpot/wiki.otpotpotp.of.by/mysql && mysqldump otpotpot_wiki > otpotpot_wiki.sql
---
+++ pre.sh
@@ -0,0 +1,1 @@
+mv LocalSettings.php LocalSettings.php_ && mv LocalSettings.php_bak LocalSettings.php
